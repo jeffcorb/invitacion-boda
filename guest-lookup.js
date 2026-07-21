82d336340217c50d9f2e1ce76fc1a25451cd5b52
jeffcorb <jefferson.corbs@gmail.com>
2026-07-20 22:20:23 -0500

    Fix guest name to Jonathan's Zapata

--- guest-lookup.js
+++ guest-lookup.js
@@ -79,7 +79,7 @@
     { name: 'Rafael Espinoza',           passes: 2 },
     { name: 'Omar Quispe',               passes: 1 },
     { name: 'Juan Camara',               passes: 1 },
-    { name: 'Jonathan Zapata',           passes: 1 },
+    { name: "Jonathan's Zapata",         passes: 1 },
   ];
 
   /* ----------------------------------------------------------
